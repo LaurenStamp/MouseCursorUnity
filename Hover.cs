c3de0dd8ef630de1ae4a9d827c06e739b3494618
LaurenStamp <47026726+LaurenStamp@users.noreply.github.com>
2019-02-02 11:19:52 +1100

    Mouse Cursor Unity
    Change the mouse cursor in Unity C#

---
+++ Hover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour {
+
+	public Texture2D mouseDefault;
+	public CursorMode curMode = CursorMode.Auto;
+	public Vector2 hotSpot = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+        // Changes the mouse cursor using the users provided image
+		Cursor.SetCursor (mouseDefault, hotSpot, curMode);
+	}
+}
